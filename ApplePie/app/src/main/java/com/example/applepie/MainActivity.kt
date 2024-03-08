b90a35f0df33de543e1d9964392621d835e3a398
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2024-03-09 02:37:53 +0700

    Create project

---
+++ ApplePie/app/src/main/java/com/example/applepie/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.applepie
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.applepie.ui.theme.ApplePieTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
