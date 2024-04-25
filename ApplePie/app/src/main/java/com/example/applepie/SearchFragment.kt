f34f14679a19952753082c1c3fb013dd71b150f6
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2024-04-25 23:49:02 +0700

    search tasks

--- ApplePie/app/src/main/java/com/example/applepie/SearchFragment.kt
+++ ApplePie/app/src/main/java/com/example/applepie/SearchFragment.kt
@@ -1,10 +1,20 @@
 package com.example.applepie
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.applepie.database.FirebaseManager
+import com.example.applepie.model.Task
+import com.google.android.material.divider.MaterialDividerItemDecoration
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +27,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
+    private lateinit var searchATCTV: AutoCompleteTextView
+    private lateinit var searchResultRV: RecyclerView
+    private lateinit var tasks: List<Task>
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -39,7 +53,84 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        tasks = FirebaseManager.getUserTask()
+        setupSearchResultRecyclerView(view)
+        setupSearchAutoCompleteTextView(view)
+    }
 
+    private fun setupSearchAutoCompleteTextView(view: View) {
+        searchATCTV = view.findViewById<AppCompatAutoCompleteTextView>(R.id.search_auto_complete_text_view) as AutoCompleteTextView
+        val autoCompleteAdapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_single_choice, tasks.map { it.title })
+        searchATCTV.setAdapter(autoCompleteAdapter)
+        searchATCTV.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (searchATCTV.text.isEmpty()) {
+                    val adapter = searchResultRV.adapter as TaskRecyclerAdapter
+                    adapter.setTasks(listOf())
+//                    if (isRecyclerView) {
+//                        val adapter = studentRV.adapter as StudentRecyclerAdapter
+//                        adapter.setStudents(students)
+//                    } else {
+//                        val adapter = studentGV.adapter as StudentGridAdapter
+//                        adapter.setStudents(students)
+//                    }
+                } else {
+                    val searchResult = tasks.filter { it.title.lowercase().contains(searchATCTV.text.toString().lowercase()) }
+                    val adapter = searchResultRV.adapter as TaskRecyclerAdapter
+                    adapter.setTasks(searchResult)
+//                    val searchResult = students.filter { it.name.lowercase().contains(searchATCTV.text.toString().lowercase()) }
+//                    if (isRecyclerView) {
+//                        val adapter = studentRV.adapter as StudentRecyclerAdapter
+//                        adapter.setStudents(searchResult)
+//                    } else {
+//                        val adapter = studentGV.adapter as StudentGridAdapter
+//                        adapter.setStudents(searchResult)
+//                    }
+                }
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (searchATCTV.text.isEmpty()) {
+                    val adapter = searchResultRV.adapter as TaskRecyclerAdapter
+                    adapter.setTasks(listOf())
+//                    if (isRecyclerView) {
+//                        val adapter = studentRV.adapter as StudentRecyclerAdapter
+//                        adapter.setStudents(students)
+//                    } else {
+//                        val adapter = studentGV.adapter as StudentGridAdapter
+//                        adapter.setStudents(students)
+//                    }
+                } else {
+                    val searchResult = tasks.filter { it.title.lowercase().contains(searchATCTV.text.toString().lowercase()) }
+                    val adapter = searchResultRV.adapter as TaskRecyclerAdapter
+                    adapter.setTasks(searchResult)
+
+//                    val searchResult = students.filter { it.name.lowercase().contains(searchATCTV.text.toString().lowercase()) }
+//                    if (isRecyclerView) {
+//                        val adapter = studentRV.adapter as StudentRecyclerAdapter
+//                        adapter.setStudents(searchResult)
+//                    } else {
+//                        val adapter = studentGV.adapter as StudentGridAdapter
+//                        adapter.setStudents(searchResult)
+//                    }
+                }
+            }
+        })
+    }
+
+    private fun setupSearchResultRecyclerView(view: View) {
+        searchResultRV = view.findViewById<RecyclerView>(R.id.search_result_recycler_view)
+
+        val adapter = TaskRecyclerAdapter(requireContext(), listOf())
+        searchResultRV.adapter = adapter
+        searchResultRV.layoutManager = LinearLayoutManager(requireContext())
+
+//        adapter.onItemClick = { task ->
+//            parentFragmentManager.beginTransaction()
+//                .replace(R.id.fragment_container, ListOfTasks.newInstance(tasks.indexOf(task)))
+//                .addToBackStack(null)
+//                .commit()
+//        }
     }
 
     companion object {
--- ApplePie/app/src/main/java/com/example/applepie/TaskRecyclerAdapter.kt
+++ ApplePie/app/src/main/java/com/example/applepie/TaskRecyclerAdapter.kt
@@ -1,5 +1,67 @@
 package com.example.applepie
 
-class TaskRecyclerAdapter {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.applepie.model.Task
+import es.dmoral.toasty.Toasty
 
+class TaskRecyclerAdapter(private val context: Context, private var tasks: List<Task>): RecyclerView.Adapter<TaskRecyclerAdapter.ViewHolder>() {
+
+    var onItemClick: ((Task) -> Unit)? = null
+
+    inner class ViewHolder(listItemView: View): RecyclerView.ViewHolder(listItemView) {
+        val taskStatusRB: RadioButton = listItemView.findViewById<RadioButton>(R.id.task_status_radio_button)
+        val taskTitleTV: TextView = listItemView.findViewById<TextView>(R.id.task_title_text_view)
+        val listNameTV: TextView = listItemView.findViewById<TextView>(R.id.list_name_text_view)
+        val dueDateTV: TextView = listItemView.findViewById<TextView>(R.id.due_date_text_view)
+
+        init {
+            listItemView.setOnClickListener {
+                onItemClick?.invoke(tasks[absoluteAdapterPosition])
+                Toast.makeText(context, "Task clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskRecyclerAdapter.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val listLayout = inflater.inflate(R.layout.layout_task, parent, false)
+        return ViewHolder(listLayout)
+    }
+
+    override fun getItemCount(): Int {
+        return tasks.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val task = tasks[position]
+        holder.taskStatusRB.isChecked = task.isDone
+        holder.taskTitleTV.text = task.title
+        holder.listNameTV.text = task.listName
+        holder.dueDateTV.text = task.due_datetime
+
+//        val color = try {
+//            task.list_color.toColor()
+//        } catch (e: IllegalArgumentException) {
+//            // Handle potential conversion errors (e.g., invalid color value)
+//            ContextCompat.getColor(context, R.color.green) // Use a default color
+//        }
+
+//        holder.iconTV.setTextColor(ContextCompat.getColor(context, R.color.green))
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setTasks(tasks: List<Task>) {
+        this.tasks = tasks
+        notifyDataSetChanged()
+    }
 }
